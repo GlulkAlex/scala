d9a5c98b620176b4c71ff1e299eb58e8ea985c22
GlulkAlex <alexanderglulkhovtsev@gmail.com>
2015-02-26 18:03:02 +0500

    week6-4(Queries with For)

--- .worksheet/src/week3.scratch.scala
+++
@@ -1,5 +0,0 @@
-package week3
-
-object scratch {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(58); val res$0 = 
-  new week3.Rational(1, 2);System.out.println("""res0: week3.Rational = """ + $show(res$0))}
-}
